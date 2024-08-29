81e4dfe4c97cddb9c0a5ed7bdf4b7fa7a8ba272c
Nirmit Deliwala <64548445+NRDeli@users.noreply.github.com>
2024-08-29 00:27:05 -0400

    homework-step-10

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ from app import add_numbers, multiply_numbers
 def test_add_numbers_pass():
     assert add_numbers(10, 5) == 15 
 
-def test_add_numbers_fail():
-    assert add_numbers(10, "5") == 15
+def test_multiply_numbers_fail():
+    assert multiply_numbers(10, "5") == 15
