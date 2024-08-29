e86c7f9d9291fa3f38757533d45b559e0905e679
Nirmit Deliwala <64548445+NRDeli@users.noreply.github.com>
2024-08-29 01:29:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ def add_numbers(a, b):
 def multiply_numbers(a, b):
     return a * b
 
-if __name__ == "__main__":
-    num1 = 10
-    num2 = "5" 
-    print(f"Sum: {add_numbers(num1, num2)}")
-    print(f"Product: {multiply_numbers(num1, num2)}")
+# if __name__ == "__main__":
+#     num1 = 10
+#     num2 = "5" 
+#     print(f"Sum: {add_numbers(num1, num2)}")
+#     print(f"Product: {multiply_numbers(num1, num2)}")
