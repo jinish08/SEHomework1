df594e4ac3e4eb713132fcf9a305d448d11dbea0
Nirmit Deliwala <64548445+NRDeli@users.noreply.github.com>
2024-08-29 02:37:10 +0000

    homework-steps-1-through-9

---
+++ app.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = "5" 
+    print(f"Sum: {add_numbers(num1, num2)}")
+    print(f"Product: {multiply_numbers(num1, num2)}")
